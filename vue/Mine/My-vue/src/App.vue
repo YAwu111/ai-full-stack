ca90df60c09b76711f96c740dd00d31d9137e447
YAwu11 <sweetsir_saoer@outlook.com>
2026-06-03 19:01:38 +0800

    My vue

---
+++ vue/Mine/My-vue/src/App.vue
@@ -0,0 +1,7 @@
+<script setup>
+import MyButton from './components/MyButton.vue'
+</script>
+
+<template>
+    <MyButton BtnName="Increase"></MyButton>
+</template>
---
+++ vue/Mine/My-vue/src/components/MyButton.vue
@@ -0,0 +1,33 @@
+<script setup>
+import { ref } from 'vue'
+const count  = ref(0);
+const props = defineProps({
+    BtnName:{
+        type:String,
+        required:true
+    }
+})
+
+const emit = defineEmits(['click']);
+function AddSelf(){
+    count.value++;
+    emit('click');
+}
+</script>
+
+<template>
+    <button type="button" @click="AddSelf">Your {{props.BtnName}} count is: {{count}}</button>
+</template>
+
+<style scoped>
+button{
+    border-radius: 100%;
+    border:1px solid white;
+    width: 150px;
+    height: 210px;
+}
+button:hover{
+    background-color: lightblue;
+    transition: all ease-in-out 0.5s;
+}
+</style>
